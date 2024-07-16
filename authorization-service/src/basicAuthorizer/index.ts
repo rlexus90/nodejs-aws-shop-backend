b0611a3409bf51d88eaf14cdfe4d2136b4eb6d8c
rlexus90 <109463776+rlexus90@users.noreply.github.com>
2024-07-16 20:38:46 +0200

    refractor: add adittional rule to verification

--- authorization-service/src/basicAuthorizer/index.ts
+++ authorization-service/src/basicAuthorizer/index.ts
@@ -6,15 +6,28 @@ export const handler = async (event: APIGatewayRequestAuthorizerEventV2) => {
       isAuthorized: false,
     };
 
-    try {
-    const credentials = event.identitySource[0].split(' ')[1];
+  try {
+    const [type, credentials] = event.identitySource[0].split(' ');
+    if (type.trim() !== 'Basic')
+      return {
+        isAuthorized: false,
+      };
+
     const encodedCredentials = atob(credentials);
+
     const [login, password] = encodedCredentials.split('=');
-
     const expectedPassword = process.env[login.trim()];
-    console.log(credentials, expectedPassword, password);
 
-    if (expectedPassword?.trim() === password.trim())
+    const [loginAdditional, passwordAdditional] = encodedCredentials.split(':');
+    const expectedPasswordAdditional = process.env[loginAdditional.trim()];
+
+    console.log(credentials, expectedPassword, password);
+    console.log(credentials, expectedPasswordAdditional, passwordAdditional);
+
+    if (
+      expectedPassword?.trim() === password.trim() ||
+      expectedPasswordAdditional?.trim() === passwordAdditional.trim()
+    )
       return { isAuthorized: true };
   } catch {
     return { isAuthorized: false };
